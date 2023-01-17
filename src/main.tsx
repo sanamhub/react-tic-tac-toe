22cc1bc8e8aff39807bb9e931ee5738e8e95f53d
Sanam <sanampakuwal27@gmail.com>
2023-01-17 09:14:50 +0545

    init

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Square from "./App";
+import "./style.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <Square />
+  </React.StrictMode>
+);
